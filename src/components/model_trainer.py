55bd5ea804ffe78cccb8fc43f33416856fdae6a7
ShivasishBC <shivasish.mahapatra@blenheimchalcot.com>
2024-07-07 19:49:32 +0530

    Model _trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -22,7 +22,7 @@ from src.utils import evaluate_model
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    trained_model_file_path = os.path.join('artifact','model.pkl')
 
 class ModelTrainer:
         def __init__(self):
