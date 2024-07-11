aecd2cf40171b1dc37d2da7a269373434567fa95
ShivasishBC <shivasish.mahapatra@blenheimchalcot.com>
2024-07-11 12:26:16 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import dill
 
 def save_object(file_path, obj):
@@ -17,20 +18,29 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)        
 
-def evaluate_model(X_train, y_train, X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params,cv=3,n_jobs=3,verbose=1,refit=False):
     try:
-        report = {}
-
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        report={}
+        for i in range(len(models)):
+            
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(X_train,y_train)
+            
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train , y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
-            report[list(models.keys())[i]] = test_model_score
+            
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            
+            train_model_score=r2_score(y_train, y_train_pred)
+            test_model_score=r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
+     
         return report    
     except:
         pass    
